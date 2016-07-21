c86ba64268b9c6f87d1004cfe054873304d97532
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2016-07-21 16:32:05 -0700

    ask all the questions and print the hash

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -3,3 +3,14 @@ client={}
 #ask user questions to collect customer data, use gets.chomp to store the answer into empty hash
 puts "What is the client's name?"
 client[:name] = gets.chomp
+
+puts "What is the client's age?"
+client[:age] = gets.chomp
+
+puts "How many children does the client have?"
+client[:number_of_children] = gets.chomp
+
+puts "What decor theme does client desire?"
+client[:decor_theme] = gets.chomp
+
+p client
