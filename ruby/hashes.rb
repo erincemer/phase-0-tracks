da625a05ae643358705b487aa5fbc69c5f28b25e
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2016-07-21 18:08:38 -0700

    create a menu with nested structure

---
+++ ruby/hashes.rb
@@ -0,0 +1,53 @@
+#create an empty hash to store the answers you will get from user as you ask
+client={}
+#ask user questions to collect customer data, use gets.chomp to store the answer into empty hash
+puts "What is the client's name?"
+client[:name] = gets.chomp
+
+puts "What is the client's age?"
+client[:age] = gets.chomp.to_i
+
+puts "How many children does the client have?"
+client[:number_of_children] = gets.chomp.to_i
+
+puts "What decor theme does client desire?"
+client[:decor_theme] = gets.chomp
+
+p client
+
+
+loop do
+
+
+	puts "Any changes you would like to make?"
+    answer = gets.chomp
+	if answer == "none"
+		puts client
+		break
+	elsif answer == "name"
+		puts "What is the name supposed to be?"
+		new_name = gets.chomp
+		client[:name] = new_name
+
+	elsif answer == "age"
+		puts "What is the age supposed to be?"
+		new_age = gets.chomp.to_i
+		client[:age] = new_age
+
+	elsif answer == "number_of_children"
+		puts "What is the number of children supposed to be?"
+		new_number_of_children = gets.chomp.to_i
+		client[:number_of_children] = new_number_of_children
+
+	elsif answer == "decor_theme"
+		puts "What is the decor theme supposed to be?"
+		new_decor_theme = gets.chomp
+		client[:decor_theme] = new_decor_theme
+
+	else
+		puts " Please type a valid answer."
+	end
+
+end
+
+p client
---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,10 @@
+menu = {
+	appetizers: ["fried calamari", "artichoke dip"],
+	entrees: {
+		from_the_ocean: ["shrimp a la carte", "tilapia scampi", "fish tacos"],
+		chicken: ["baked garlic chicken", "chicken parmesan", "chicken enchiladas"],
+		vegetarian: ["homemade veggie pizza", "spinach enchiladas", "thai tofu"]
+	},
+	dessert: ["chocolate lava cake", "cheesecake"],
+	drinks: []
+}
