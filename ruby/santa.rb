403896eb27048a5233d030f29c5437c0819c2eb1
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2016-07-26 14:48:46 -0700

    create the santa class

---
+++ ruby/santa.rb
@@ -0,0 +1,16 @@
+#6.3
+class Santa
+
+  def initialize
+    puts "Initializing Santa instance..."
+  end
+
+  def speak
+    puts "Ho, ho, ho! Haaaapy holidays!"
+  end
+
+  def eat_milk_and_cookies(cookie)
+    puts "That was a good #{cookie}."
+  end
+
+end
