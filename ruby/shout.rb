e8af80d12480f9ddedbda1f7155dff545a023788
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2016-07-26 16:32:57 -0700

    try out new methods in module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,12 @@
 module Shout
-  
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yelling_happily(n)
+    n.times {puts "That's what I'm talking about!"}
+  end
 end
+
+#Shout.yell_angrily("No")
+#Shout.yelling_happily(3)
