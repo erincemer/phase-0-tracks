7fd91f1a55276c63c640678b1290be6e25144069
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2016-07-26 15:44:17 -0700

    done with release 4

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,8 +1,13 @@
-#6.3
 class Santa
+   attr_reader :age, :ethnicity
+   attr_accessor:gender
 
-  def initialize
+  def initialize(gender, ethnicity)
     puts "Initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
 
   def speak
@@ -13,4 +18,27 @@ class Santa
     puts "That was a good #{cookie}."
   end
 
+  def celebrate_birthday
+  	@age += 1
+  end
+
+  def get_mad_at(reindeer)
+  	@reindeer_ranking.delete(reindeer)
+  	@reindeer_ranking.push(reindeer)
+  	p @reindeer_ranking
+  end
+
 end
+
+#santa = Santa.new("male", "black")
+#santa.speak
+#santa.eat_milk_and_cookies("muffin")
+##santa.get_mad_at("Vixen")
+#santa.celebrate_birthday
+#santa.celebrate_birthday
+#
+#p santa.age
+#p santa.ethnicity
+#p santa.gender
+#santa.gender = "female"
+#p santa.gender
