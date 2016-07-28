65adfe22097b43fbbf31f1112d57af995ed8ad39
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2016-07-28 13:46:35 -0700

    pseduecode release1

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,30 @@
+#RELEASE 1
+
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps:
+  # splitting the string to have array of strings
+  # set default quantity
+  # iterate through the array to form the hash
+  # print the list to the console [can you use one of your other methods here?]
+# output: hash
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps: assign the item to the hash we create in create_list method as key-value  pair
+# output: hash
+
+# Method to remove an item from the list
+# input: item name
+# steps: delete method to remove the item from hash
+# output: hash
+
+# Method to update the quantity of an item
+# input: item name and quantity
+# steps: assign the new value for the item(key),like overwriting
+# output: hash
+
+# Method to print a list and make it look pretty
+# input: no parameter
+# steps: each method on our hash to print some sentence using the key #{} and value
+# output: strings(sentences)
