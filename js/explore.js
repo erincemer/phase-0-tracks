fd80fc554c1a609d7d1528f9f10d18a57e1ddb64
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2016-08-01 21:41:57 -0700

    add test code

--- js/explore.js
+++ js/explore.js
@@ -1,3 +1,15 @@
 //create function to reverse a string that given as parameter
 // split the string so we can have an array with letters
-//"for each" letter we unshift to the new empty array
+//"for each" letter we unshift to the new empty array and return the new
+//array after join the letters
+
+function reverse(str){
+  var letters = str.split("");
+  var arr = [];
+  for(var i = 0; i < letters.length; i ++){
+    arr.unshift(letters[i]);
+  }
+  return arr.join("")
+}
+
+reverse("hello")
