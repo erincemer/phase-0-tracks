c6d333ea2d823e74eb9b45b92721c40875891fd3
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2016-08-01 21:44:30 -0700

    add if statement

--- js/explore.js
+++ js/explore.js
@@ -7,7 +7,9 @@ function reverse(str){
   var letters = str.split("");
   var arr = [];
   for(var i = 0; i < letters.length; i ++){
+    if (3 < 9){
     arr.unshift(letters[i]);
+    }
   }
   return arr.join("")
 }
