f5dfdc6307adfae9ae7ba99d00b3424f53449743
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2016-08-04 16:46:17 -0700

    done with release2

--- js/algos.js
+++ js/algos.js
@@ -9,5 +9,20 @@ function longest(arr){
 	return longestWord;
 }
 
-longest(["long phrase","longest phrase","longer phrase"])
-longest(["pen", "computer", "genie"])
+//RELEASE 1
+function comparePairs(obj1, obj2){
+	var keys1 = Object.keys(obj1);
+	var keys2 = Object.keys(obj2);
+	for(var i = 0; i < keys1.length; i ++){
+		if (keys1[i] === keys2[i] || obj1[keys1[i]] === obj2[keys2[i]]){
+			return true
+		}
+		return false
+	}
+}
+
+comparePairs({name: "Steven", age: 54}, {name: "Tamir", age: 44})
+comparePairs({location: "Napa", age: 54}, {currency: "euro", continent: "Europe"})
+
+//longest(["long phrase","longest phrase","longer phrase"])
+//longest(["pen", "computer", "genie"])
