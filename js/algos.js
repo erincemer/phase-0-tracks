c91e6a2d48f3e655334507a38a9447507d3aa10b
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2016-08-05 08:35:12 -0700

    done wth release 2

--- js/algos.js
+++ js/algos.js
@@ -1,4 +1,7 @@
 //RELEASE 0
+//create an empty string variable so we can call assign to the longest word
+//as we iterate the array.Compare the lengths of each item in the array.
+
 function longest(arr){
 	var longestWord = "";
 	for(var i = 0; i < arr.length; i++){
@@ -10,6 +13,8 @@ function longest(arr){
 }
 
 //RELEASE 1
+//Object.keys method to assign the keys of the objects so it would be easy to read.
+//iterate each objects to check if their keys or values match
 function comparePairs(obj1, obj2){
 	var keys1 = Object.keys(obj1);
 	var keys2 = Object.keys(obj2);
@@ -21,8 +26,38 @@ function comparePairs(obj1, obj2){
 	}
 }
 
+//RELEASE 2
+//One loop that creates random letters using Math.random() and alphabet
+//string then add(+) letters together to create a word(string). Another
+//loop that creates n amount of strings into array.
+function make_string(n){
+	var arr = [];
+	for(var j=0; j < n; j++) {
+    	var text = "";
+    	var letters = "abcdefghijklmnopqrstuvwxyz";
+
+    	for( var i=0; i < Math.floor(Math.random() * 10) +2; i++ ){
+    	    text += letters.charAt(Math.floor(Math.random() * letters.length));
+
+			 }
+			 arr.push(text);
+	}
+    return arr;
+}
+
+// DRIVER CODES
+
+for(var i = 0; i < 11; i ++){
+	var arr = make_string(3);
+	console.log(arr)
+	console.log(longest(arr))
+}
+
+make_string(3);
+
+
 comparePairs({name: "Steven", age: 54}, {name: "Tamir", age: 44})
 comparePairs({location: "Napa", age: 54}, {currency: "euro", continent: "Europe"})
 
-//longest(["long phrase","longest phrase","longer phrase"])
-//longest(["pen", "computer", "genie"])
+longest(["long phrase","longest phrase","longer phrase"])
+longest(["pen", "computer", "genie"])
