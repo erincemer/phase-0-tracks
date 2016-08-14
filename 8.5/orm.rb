ee1c4ccc99b495307385cc594a424fea67a101ba
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2016-08-14 12:25:38 -0700

    edit

--- 8.5/orm.rb
+++ 8.5/orm.rb
@@ -37,20 +37,44 @@ end
 # explore ORM by retrieving data
 
 def display(db)
+  puts"-------------"
   beers = db.execute("SELECT * FROM beer_menu")
 
   beers.each do |beer|
     puts"-#{beer["name"]}, a #{beer["style"]}, contains #{beer["alcohol"]} alcohol and will be sold for #{beer["price"]}$."
   end
+  puts"-------------"
 end
 #display(db)
 
-def update_menu
+def delete(db, name)
+  db.execute("DELETE from beer_menu WHERE name=?",[name])
+  display(db)
+end
+
+delete(db, "Duvel")
+
+def update_menu(db)
+
   puts"--------------"
   puts"What would you like to update in the beer menu?"
   input = gets.chomp
   loop do
-    until inp
+    if input == "done"
+      display(db)
+      break
+    elsif input =="name"
+      puts"Which beer name you would like to edit?"
+      name_to_edit=gets.chomp
+      #puts"What should be the correct beer name?"
+      #beer=gets.chomp
+      db.execute("UPDATE beer_menu SET name='tuborg' where name='name_to_edit'")
+      display(db)
+      break
+    else
+
+    end
 
   end
 end
+#update_menu(db)
